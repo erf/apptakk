221deec7c31c981057b8446e1a0500ba8f3d3a65
Erlend Lind Madsen <erlendf80@gmail.com>
2026-05-01 17:27:34 +0700

    data formatted

--- data.js
+++ data.js
@@ -1,138 +1,138 @@
 const apps = [
-	{
-		name: 'Cashier',
-		description: 'Minimal budget app',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id983409234', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Cashplan',
-		description: 'Plan ahead',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id1500199497', icon: 'images/Apple-50.png', },
-			{ src: 'https://play.google.com/store/apps/details?id=com.apptakk.cashplan', icon: 'images/Android-OS-50.png', },
-			{ src: 'https://www.youtube.com/watch?v=DjHUDcpSRX8', icon: 'images/icons8-youtube.png', },
-		]
-	},
-	{
-		name: 'Kanbanero',
-		description: 'Kanban Tasks Lite',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id1645553955', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Countess',
-		description: 'Tap to Count',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id988588637', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Frem',
-		description: 'Journey planner for Norway',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id985450528', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Coinport',
-		description: 'Altcoin Portfolio',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id1252011703', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Bedriftssøk',
-		description: 'Søk opp bedrifter i brreg.no',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id1352412060', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Currency',
-		description: 'Convert between currencies',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id1278416094', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Forget Not',
-		description: 'Minimal reminder app',
-		urls: [
-			{ src: 'https://apps.apple.com/app/id1249724822', icon: 'images/Apple-50.png', },
-		]
-	},
-	{
-		name: 'Atelie',
-		description: 'Art platform (Freelance 2023)',
-		urls: [
-			{ src: 'https://apps.apple.com/app/atelie/id1629244383', icon: 'images/Apple-50.png', },
-			{ src: 'https://play.google.com/store/apps/details?id=art.atelie.atelier&pli=1', icon: 'images/Android-OS-50.png', },
-			{ src: 'https://atelie.art', icon: 'images/icons8-globe.png', },
-		]
-	},
-	{
-		name: 'Eple',
-		description: 'Local food platform',
-		urls: [
-			{ src: 'https://eple.io/', icon: 'images/icons8-globe.png', },
-		]
-	},
+    {
+        name: 'Cashier',
+        description: 'Minimal budget app',
+        urls: [
+            { src: 'https://apps.apple.com/app/id983409234', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Cashplan',
+        description: 'Plan ahead',
+        urls: [
+            { src: 'https://apps.apple.com/app/id1500199497', icon: 'images/Apple-50.png', },
+            { src: 'https://play.google.com/store/apps/details?id=com.apptakk.cashplan', icon: 'images/Android-OS-50.png', },
+            { src: 'https://www.youtube.com/watch?v=DjHUDcpSRX8', icon: 'images/icons8-youtube.png', },
+        ]
+    },
+    {
+        name: 'Kanbanero',
+        description: 'Kanban Tasks Lite',
+        urls: [
+            { src: 'https://apps.apple.com/app/id1645553955', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Countess',
+        description: 'Tap to Count',
+        urls: [
+            { src: 'https://apps.apple.com/app/id988588637', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Frem',
+        description: 'Journey planner for Norway',
+        urls: [
+            { src: 'https://apps.apple.com/app/id985450528', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Coinport',
+        description: 'Altcoin Portfolio',
+        urls: [
+            { src: 'https://apps.apple.com/app/id1252011703', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Bedriftssøk',
+        description: 'Søk opp bedrifter i brreg.no',
+        urls: [
+            { src: 'https://apps.apple.com/app/id1352412060', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Currency',
+        description: 'Convert between currencies',
+        urls: [
+            { src: 'https://apps.apple.com/app/id1278416094', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Forget Not',
+        description: 'Minimal reminder app',
+        urls: [
+            { src: 'https://apps.apple.com/app/id1249724822', icon: 'images/Apple-50.png', },
+        ]
+    },
+    {
+        name: 'Atelie',
+        description: 'Art platform (Freelance 2023)',
+        urls: [
+            { src: 'https://apps.apple.com/app/atelie/id1629244383', icon: 'images/Apple-50.png', },
+            { src: 'https://play.google.com/store/apps/details?id=art.atelie.atelier&pli=1', icon: 'images/Android-OS-50.png', },
+            { src: 'https://atelie.art', icon: 'images/icons8-globe.png', },
+        ]
+    },
+    {
+        name: 'Eple',
+        description: 'Local food platform',
+        urls: [
+            { src: 'https://eple.io/', icon: 'images/icons8-globe.png', },
+        ]
+    },
 ]
 
 const repos = [
-	{
-		name: 'vid',
-		description: 'vi-like editor in Dart ',
-		urls: [
-			{ src: 'https://github.com/erf/vid', icon: 'images/icons8-github.png', },
-		]
-	},
-	{
-		name: 'vis-plug',
-		description: 'plugin-manager for vis',
-		urls: [
-			{ src: 'https://github.com/erf/vis-plug', icon: 'images/icons8-github.png', },
-		]
-	},
-	{
-		name: 'mu_state',
-		description: 'Pragmatic state in Flutter',
-		urls: [
-			{ src: 'https://github.com/erf/mu_state', icon: 'images/icons8-github.png' },
-			{ src: 'https://pub.dev/packages/mu_state', icon: 'images/icons8-globe.png', },
-		]
-	},
-	{
-		name: 'csv_localizations',
-		description: 'CSV localization for Flutter',
-		urls: [
-			{ src: 'https://github.com/erf/csv_localizations', icon: 'images/icons8-github.png', },
-			{ src: 'https://pub.dev/packages/csv_localizations', icon: 'images/icons8-globe.png', },
-		]
-	},
-	{
-		name: 'flim',
-		description: 'Sprite render in Flutter',
-		urls: [
-			{ src: 'https://github.com/erf/flim', icon: 'images/icons8-github.png', },
-			{ src: 'https://pub.dev/packages/flim', icon: 'images/icons8-globe.png', },
-		]
-	},
-	{
-		name: 'node-resize',
-		description: 'Image resize server',
-		urls: [
-			{ src: 'https://github.com/erf/node-resize', icon: 'images/icons8-github.png', },
-		]
-	},
-	{
-		name: 'elite',
-		description: 'Declarative JS DOM',
-		urls: [
-			{ src: 'https://github.com/erf/elite', icon: 'images/icons8-github.png', },
-		]
-	},
+    {
+        name: 'vid',
+        description: 'vi-like editor in Dart ',
+        urls: [
+            { src: 'https://github.com/erf/vid', icon: 'images/icons8-github.png', },
+        ]
+    },
+    {
+        name: 'vis-plug',
+        description: 'plugin-manager for vis',
+        urls: [
+            { src: 'https://github.com/erf/vis-plug', icon: 'images/icons8-github.png', },
+        ]
+    },
+    {
+        name: 'mu_state',
+        description: 'Pragmatic state in Flutter',
+        urls: [
+            { src: 'https://github.com/erf/mu_state', icon: 'images/icons8-github.png' },
+            { src: 'https://pub.dev/packages/mu_state', icon: 'images/icons8-globe.png', },
+        ]
+    },
+    {
+        name: 'csv_localizations',
+        description: 'CSV localization for Flutter',
+        urls: [
+            { src: 'https://github.com/erf/csv_localizations', icon: 'images/icons8-github.png', },
+            { src: 'https://pub.dev/packages/csv_localizations', icon: 'images/icons8-globe.png', },
+        ]
+    },
+    {
+        name: 'flim',
+        description: 'Sprite render in Flutter',
+        urls: [
+            { src: 'https://github.com/erf/flim', icon: 'images/icons8-github.png', },
+            { src: 'https://pub.dev/packages/flim', icon: 'images/icons8-globe.png', },
+        ]
+    },
+    {
+        name: 'node-resize',
+        description: 'Image resize server',
+        urls: [
+            { src: 'https://github.com/erf/node-resize', icon: 'images/icons8-github.png', },
+        ]
+    },
+    {
+        name: 'elite',
+        description: 'Declarative JS DOM',
+        urls: [
+            { src: 'https://github.com/erf/elite', icon: 'images/icons8-github.png', },
+        ]
+    },
 ]
